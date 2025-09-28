dec69678efc1ccb775001dd1ee596e3147b519ef
AnuragPhatak <anuragamey123@gmail.com>
2025-09-28 23:20:59 +0530

    project done

--- src/api.py
+++ src/api.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import mlflow.pyfunc
 
-
 MODEL_NAME = "ChurnPredictionModel"
 MODEL_STAGE = "Production"  
 model = mlflow.pyfunc.load_model(model_uri=f"models:/{MODEL_NAME}/{MODEL_STAGE}")
